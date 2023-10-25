1b0c2d267a472d3f3940f8de011f844488e90ae5
eng.ahmedkhatab84 <eng.ahmedkhatab84@gmail.com>
2023-10-25 22:52:18 +0300

    miroservice multi-module project skeleton

---
+++ src/main/java/com/amsoft/microservice/fullmicroserviceapp/FullMicroserviceAppApplication.java
@@ -0,0 +1,13 @@
+package com.amsoft.microservice.fullmicroserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullMicroserviceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullMicroserviceAppApplication.class, args);
+	}
+
+}
