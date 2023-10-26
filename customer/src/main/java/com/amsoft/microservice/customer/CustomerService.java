18adee78da4b68424c2f7d2fbfbeb74459b4cd00
eng.ahmedkhatab84 <eng.ahmedkhatab84@gmail.com>
2023-10-26 22:30:16 +0300

    Fraud Service

--- customer/src/main/java/com/amsoft/microservice/customer/CustomerService.java
+++ customer/src/main/java/com/amsoft/microservice/customer/CustomerService.java
@@ -3,6 +3,7 @@ package com.amsoft.microservice.customer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -10,13 +11,26 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
 
     private final CustomerRepo customerRepo;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerDto customerDto){
         Customer customer = new Customer();
         customer.setFirstName(customerDto.firstName());
         customer.setLastName(customerDto.lastName());
         customer.setEmail(customerDto.email());
-        customerRepo.save(customer);
+        // use saveAndFlush to get id
+        customerRepo.saveAndFlush(customer);
+
+        FraudCheckResponse response = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{id}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if(response.isFraudster()){
+            throw new RuntimeException("Fraud Customer Exception");
+        }
+
     }
 
 }
---
+++ customer/src/main/java/com/amsoft/microservice/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.amsoft.microservice.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
